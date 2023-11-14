481211d5d7936835885792f6a4390058205584e0
Hikmat Yahya <Hikmat.yahya@hotmail.com>
2023-11-14 09:03:15 +0300

    Add login

--- Atcco/Constants/FileUploadConstants.cs
+++ Atcco/Constants/FileUploadConstants.cs
@@ -3,7 +3,7 @@
     public class FileUploadConstants
     {
 
-        public const string UploadFolderPath = @"C:\Users\hikma\source\repos\Atcco1\Atcco\Atcco\wwwroot\img\"; // Replace with your actual path
+        public const string UploadFolderPath = @"C:\Users\hikmat.shehadi\Source\Repos\Atcco1\Atcco\wwwroot\img\"; // Replace with your actual path
 		//C:\Users\hikma\source\repos\Atcco1\Atcco\Atcco\wwwroot\img\																										 //C:\Users\hikmat.shehadi\source\repos\Atcco\Atcco\wwwroot\uploads\
 	}
 }
